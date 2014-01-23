7f488d32b1bd053841fac3defc3beacb19872e9d
Ryan Grove <ryan@wonko.com>
2014-01-22 19:51:21 -0800

    Fix off-by-one error. Reformat.

--- cluster.js
+++ cluster.js
@@ -1,23 +1,24 @@
 #!/usr/bin/env node
-
 /*jshint node:true */
+
+"use strict";
+
 var cluster = require('cluster');
-var numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
-  // Start the workers
-  for (var i = numCPUs; i >= 0; i--) {
-    cluster.fork();
-  }
+    var workerCount = require('os').cpus().length;
 
-  cluster.on('exit', function(deadWorker) {
-    // revive
-    var worker = cluster.fork();
+    while (workerCount--) {
+        cluster.fork();
+    }
 
-    // log the dead worker
-    console.log('worker ' + deadWorker.process.pid + ' replaced by ' + worker.process.pid);
-  });
+    cluster.on('exit', function (deadWorker) {
+        var worker = cluster.fork();
+
+        console.log('worker ' + deadWorker.process.pid + ' replaced by ' +
+            worker.process.pid);
+    });
 } else {
-  console.log('Starting worker ' + process.pid);
-  require('./web');
+    console.log('Starting worker ' + process.pid);
+    require('./web');
 }
