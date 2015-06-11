2ad3cb639ec7ddb14d658b350aa477b725808144
Kevin Ingersoll <kingersoll@gmail.com>
2015-06-11 14:56:29 -0700

    Test directory index for content type

--- test/test.routes.js
+++ test/test.routes.js
@@ -249,6 +249,7 @@ describe("repo", function () {
     it("should return index.html when path is a directory, cacheable for 5 minutes", function (done) {
       agent.get('/jekyll/jekyll/gh-pages/')
         .expect(200, 'Hello world!')
+        .expect('content-type', 'text/html;charset=utf-8')
         .expect('cache-control', 'max-age=300')
         .end(done);
     });
