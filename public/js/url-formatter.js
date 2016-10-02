c378c0103a334a9a65dc9553c50f65603892ef7e
Ryan Grove <ryan@wonko.com>
2016-10-01 20:33:14 -0700

    Look up raw gist URL via the API when a non-raw URL is pasted
    People had a lot of trouble with this since it wasn't obvious that
    RawGit needed the raw gist URL (and the raw gist URL couldn't be
    derived locally from the non-raw URL). Hopefully this makes it easier.

--- public/js/url-formatter.js
+++ public/js/url-formatter.js
@@ -2,92 +2,140 @@
 (function (doc) {
   "use strict";
 
-  var REGEX_GIST_URL = /^(https?):\/\/gist\.github(?:usercontent)?\.com\/(.+?\/[0-9a-f]+\/raw\/(?:[0-9a-f]+\/)?.+\..+)$/i;
-  var REGEX_RAW_URL  = /^(https?):\/\/raw\.github(?:usercontent)?\.com\/([^\/]+\/[^\/]+\/[^\/]+|[0-9A-Za-z-]+\/[0-9a-f]+\/raw)\/(.+\..+)/i;
-  var REGEX_REPO_URL = /^(https?):\/\/github\.com\/(.[^\/]+?)\/(.[^\/]+?)\/(?!releases\/)(?:(?:blob|raw)\/)?(.+?\/.+)/i;
+  var GITHUB_API_URL = 'https://api.github.com';
 
-  var devEl  = doc.getElementById('url-dev');
-  var prodEl = doc.getElementById('url-prod');
-  var urlEl  = doc.getElementById('url');
+  var REGEX_GIST_URL     = /^https?:\/\/gist\.github\.com\/.+?\/([0-9a-f]+)(?:\/([0-9a-f]+))?/i;
+  var REGEX_RAW_GIST_URL = /^https?:\/\/gist\.githubusercontent\.com\/(.+?\/[0-9a-f]+\/raw\/(?:[0-9a-f]+\/)?.+\..+)$/i;
+  var REGEX_RAW_REPO_URL = /^https?:\/\/raw\.github(?:usercontent)?\.com\/([^\/]+\/[^\/]+\/[^\/]+|[0-9A-Za-z-]+\/[0-9a-f]+\/raw)\/(.+\..+)/i;
+  var REGEX_REPO_URL     = /^https?:\/\/github\.com\/(.[^\/]+?)\/(.[^\/]+?)\/(?!releases\/)(?:(?:blob|raw)\/)?(.+?\/.+)/i;
+
+  var copyButtonDev  = doc.getElementById('url-dev-copy');
+  var copyButtonProd = doc.getElementById('url-prod-copy');
+  var inputDev       = doc.getElementById('url-dev');
+  var inputProd      = doc.getElementById('url-prod');
+  var inputUrl       = doc.getElementById('url');
 
   new Clipboard('.url-copy-button');
 
-  var devCopyButton  = doc.getElementById('url-dev-copy');
-  var prodCopyButton = doc.getElementById('url-prod-copy');
-
   if (doc.queryCommandSupported && doc.queryCommandSupported('copy')) {
-    devCopyButton.style.display  = 'inline-block';
-    prodCopyButton.style.display = 'inline-block';
+    copyButtonDev.style.display  = 'inline-block';
+    copyButtonProd.style.display = 'inline-block';
   }
 
-  urlEl.addEventListener('input', formatURL, false);
-  formatURL();
+  inputDev.addEventListener('focus', onInputFocus);
+  inputProd.addEventListener('focus', onInputFocus);
+  inputUrl.addEventListener('input', formatUrl);
 
-  function formatURL () {
-    var url = urlEl.value.trim();
+  formatUrl();
 
-    if (REGEX_RAW_URL.test(url)) {
-      urlEl.classList.remove('invalid');
-      urlEl.classList.add('valid');
+  window.handleGistResponse = function (res) {
+    if (!res) {
+      return void setInvalid();
+    }
 
-      devEl.value  = url.replace(REGEX_RAW_URL, 'https://' + devDomain + '/$2/$3');
-      prodEl.value = url.replace(REGEX_RAW_URL, 'https://' + cdnDomain + '/$2/$3');
+    var status = res.meta && res.meta.status;
 
-      devEl.classList.add('valid');
-      prodEl.classList.add('valid');
+    if (status !== 200) {
+      return void setInvalid();
+    }
 
-      devCopyButton.disabled  = false;
-      prodCopyButton.disabled = false;
-    } else if (REGEX_REPO_URL.test(url)) {
-      urlEl.classList.remove('invalid');
-      urlEl.classList.add('valid');
+    var files     = res.data && res.data.files;
+    var filenames = files && Object.keys(res.data.files);
 
-      devEl.value  = url.replace(REGEX_REPO_URL, 'https://' + devDomain + '/$2/$3/$4');
-      prodEl.value = url.replace(REGEX_REPO_URL, 'https://' + cdnDomain + '/$2/$3/$4');
+    if (!filenames || !filenames.length) {
+      return void setInvalid();
+    }
 
-      devEl.classList.add('valid');
-      prodEl.classList.add('valid');
+    var rawUrl = files[filenames[0]] && files[filenames[0]].raw_url;
 
-      devCopyButton.disabled  = false;
-      prodCopyButton.disabled = false;
-    } else if (REGEX_GIST_URL.test(url)) {
-      urlEl.classList.remove('invalid');
-      urlEl.classList.add('valid');
-
-      devEl.value  = url.replace(REGEX_GIST_URL, 'https://' + devDomain + '/$2');
-      prodEl.value = url.replace(REGEX_GIST_URL, 'https://' + cdnDomain + '/$2');
-
-      devEl.classList.add('valid');
-      prodEl.classList.add('valid');
-
-      devCopyButton.disabled  = false;
-      prodCopyButton.disabled = false;
+    if (rawUrl) {
+      formatRawGistUrl(rawUrl);
     } else {
-      urlEl.classList.remove('valid');
+      setInvalid();
+    }
+  };
 
-      if (url.length) {
-        urlEl.classList.add('invalid');
-      } else {
-        urlEl.classList.remove('invalid');
-      }
+  function formatRawGistUrl(url) {
+    inputDev.value  = url.replace(REGEX_RAW_GIST_URL, 'https://' + devDomain + '/$1');
+    inputProd.value = url.replace(REGEX_RAW_GIST_URL, 'https://' + cdnDomain + '/$1');
 
-      devEl.value  = '';
-      prodEl.value = '';
+    setValid();
+  }
 
-      devEl.classList.remove('valid');
-      prodEl.classList.remove('valid');
+  function formatRawRepoUrl(url) {
+    inputDev.value  = url.replace(REGEX_RAW_REPO_URL, 'https://' + devDomain + '/$1/$2');
+    inputProd.value = url.replace(REGEX_RAW_REPO_URL, 'https://' + cdnDomain + '/$1/$2');
 
-      devCopyButton.disabled  = true;
-      prodCopyButton.disabled = true;
+    setValid();
+  }
+
+  function formatRepoUrl(url) {
+    inputDev.value  = url.replace(REGEX_REPO_URL, 'https://' + devDomain + '/$1/$2/$3');
+    inputProd.value = url.replace(REGEX_REPO_URL, 'https://' + cdnDomain + '/$1/$2/$3');
+
+    setValid();
+  }
+
+  function formatUrl() {
+    var url = inputUrl.value.trim();
+
+    if (REGEX_RAW_REPO_URL.test(url)) {
+      formatRawRepoUrl(url);
+    } else if (REGEX_RAW_GIST_URL.test(url)) {
+      formatRawGistUrl(url);
+    } else if (REGEX_REPO_URL.test(url)) {
+      formatRepoUrl(url);
+    } else if (REGEX_GIST_URL.test(url)) {
+      requestGistUrl(url);
+    } else {
+      setInvalid();
     }
   }
 
-  devEl.addEventListener('focus', onFocus);
-  prodEl.addEventListener('focus', onFocus);
-
-  function onFocus(e) {
+  function onInputFocus(e) {
     setTimeout(function () {
       e.target.select();
     }, 1);
+  }
+
+  function requestGistUrl(url) {
+    var matches = url.match(REGEX_GIST_URL);
+    var script  = doc.createElement('script');
+
+    script.src = GITHUB_API_URL + '/gists/' + matches[1]
+      + (matches[2] ? '/' + matches[2] : '')
+      + '?callback=handleGistResponse';
+
+    doc.head.appendChild(script);
+  }
+
+  function setInvalid() {
+    copyButtonDev.disabled  = true;
+    copyButtonProd.disabled = true;
+
+    inputUrl.classList.remove('valid');
+
+    if (inputUrl.value.trim().length) {
+      inputUrl.classList.add('invalid');
+    } else {
+      inputUrl.classList.remove('invalid');
+    }
+
+    inputDev.value  = '';
+    inputProd.value = '';
+
+    inputDev.classList.remove('valid');
+    inputProd.classList.remove('valid');
+  }
+
+  function setValid() {
+    copyButtonDev.disabled  = false;
+    copyButtonProd.disabled = false;
+
+    inputUrl.classList.remove('invalid');
+
+    inputDev.classList.add('valid');
+    inputProd.classList.add('valid');
+    inputUrl.classList.add('valid');
   }
 }(document));
